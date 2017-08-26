00222cbf267a02bf55be75a6a257ff65eaf1ad27
ashkan-rmk <ashkan.rmk@hotmail.com>
2017-08-26 21:59:27 +0430

    Seeding Users and Roles

---
+++ Vidly/Models/RoleName.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public static class RoleName
+    {
+        public const string CanManageMovies = "CanManageMovies";
+    }
+}
